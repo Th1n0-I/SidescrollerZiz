809e24b86807bab5a294ffff07aa0d7585944c1d
Th1n0-I <thinoivarsson7@gmail.com>
2025-11-16 17:17:13 +0100

    feat: particleSystem

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 
 public class Pickup : MonoBehaviour
 {
-    public Counters counters;
+    public                   Counters   counters;
+    [SerializeField] private GameObject particles;
 
     private void Start() {
         counters = FindAnyObjectByType<Counters>();
@@ -12,6 +13,7 @@ public class Pickup : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.CompareTag("Player")) {
             counters.AddDiamonds();
+            GameObject.Instantiate(particles, transform.position, Quaternion.identity);
             Destroy(gameObject);
             FindAnyObjectByType<AudioManager>().PlaySound(3);
         }
